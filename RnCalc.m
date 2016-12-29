af35b04d8f2f590ebb80bcbe9a23170c10a99a99
cpobes <carlos.pobes@gmail.com>
2016-12-29 13:08:00 +0100

    update RnCalc
    -pasar circuit en lugar de Rf

--- RnCalc.m
+++ RnCalc.m
@@ -1,11 +1,15 @@
-function Rn=RnCalc(mN,mS,Rf)
+function Rn=RnCalc(mN,mS,circuit)
 %calcula la Rn a partir de las pendientes en estado normal y
 %superconductor.
 %OJO, la Rf del fichero S puede ser distinta de la del fichero N.
 
-Rpar=RparCalc(mS,Rf)
-Rsh=2e-3;
-invMs=24.1;
-invMf=66;
-Rn=(Rsh*Rf*invMf/(mN*invMs)-Rsh-Rpar);
+Rpar=RparCalc(mS,circuit)
+% Rsh=2e-3;
+% invMs=24.1;
+% invMf=66;
+invMf=circuit.invMf;%66;
+invMin=circuit.invMin;%24.1;
+Rsh=circuit.Rsh;%2e-3;
+Rf=circuit.Rf;
+Rn=(Rsh*Rf*invMf/(mN*invMin)-Rsh-Rpar);
 
