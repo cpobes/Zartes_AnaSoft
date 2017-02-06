52b0706aec95a929423cd15f742dfaae59402ce5
cpobes <carlos.pobes@gmail.com>
2017-02-06 22:25:45 +0100

    cambios para simulacion y ajustes P
    -cambio BuildIVsimStruct para aceptar IVset
    -añado modelo 2 a fitP
    -fitPvsTset cambio la forma de pasar fit.param
    -corrijo modelo 'ere' en FtesTI
    -modifico GetGfromFit para aceptar nuevo fit

--- GetGfromFit.m
+++ GetGfromFit.m
@@ -1,7 +1,13 @@
 function param=GetGfromFit(fit)
+%%Devuelve los parámetros termicos a partir de un fit
+% param.n=fit.b;
+% param.K=-fit.a;
+% param.Tc=(fit.c/-fit.a)^(1/fit.b);
 
-param.n=fit.b;
-param.K=-fit.a;
-param.Tc=(fit.c/-fit.a)^(1/fit.b)
+param.n=fit(2);
+param.K=-fit(1);
+param.Tc=(fit(3)/-fit(1))^(1/fit(2));
+param.Ic=fit(4);
+
 param.G=param.n*param.K*param.Tc^(param.n-1);
 
--- fitPvsTset.m
+++ fitPvsTset.m
@@ -6,23 +6,27 @@ for i=1:length(IVTESset), Tbath(i)=IVTESset(i).Tbath;end
 
 for jj=1:length(perc)
     Paux=[];
+    Iaux=[];
     for i=1:length(Tbath) 
         %txt=strcat('P',num2str(100*perc(jj)));
         %exec=strcat(txt,'(i)=','ppval(spline(IVTESset{i}.rtes,IVTESset{i}.ptes),jj)')
         %evalin('caller',exec);
         ind=find(IVTESset(i).rtes>0.15&IVTESset(i).rtes<0.95);%%%algunas IVs fallan.
         Paux(i)=ppval(spline(IVTESset(i).rtes(ind),IVTESset(i).ptes(ind)),perc(jj));
+        Iaux(i)=ppval(spline(IVTESset(i).rtes(ind),IVTESset(i).ites(ind)),perc(jj));%%%
     end
     %fitaux=fit(Tbath',Paux'*1e12,'a*x^b+c','startpoint',[0 3 0]);
     %Tbath
     %Paux
     plot(Tbath,Paux*1e12,'.'),hold on
-    fit=lsqcurvefit(@fitP,[-3500 3 1],Tbath,Paux*1e12);
-    plot(Tbath,fitP(fit,Tbath),'-r')
-    fitaux.a=fit(1);
-    fitaux.b=fit(2);
-    fitaux.c=fit(3);
-    Gaux(jj)=GetGfromFit(fitaux);
+    %X0=[-3500 3 1];XDATA=Tbath;
+    X0=[-5000 3.0 10 1e4]; XDATA=[Tbath;Iaux*1e6];LB=[-Inf 2 0 0];
+    fit=lsqcurvefit(@fitP,X0,XDATA,Paux*1e12,LB);
+    plot(Tbath,fitP(fit,XDATA),'-r')
+%     fitaux.a=fit(1);
+%     fitaux.b=fit(2);
+%     fitaux.c=fit(3);
+    Gaux(jj)=GetGfromFit(fit);%%antes se pasaba fitaux.
 end
 for jj=1:length(perc) Gaux(jj).rp=perc(jj);end
 xlabel('T_{bath}(K)','fontsize',11,'fontweight','bold')
