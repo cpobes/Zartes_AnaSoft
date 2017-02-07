148d6f743087776eb742bb4dddfc4f5883404461
cpobes <carlos.pobes@gmail.com>
2017-02-07 09:51:55 +0100

    Update plotTESR_T.m

--- plotTESR_T.m
+++ plotTESR_T.m
@@ -1,6 +1,6 @@
 function plotTESR_T(Circuit,IVmeasure,TES)
 %%pinta la R(T) de un TES a partir de una curva IV pasada como estructura.
-%%Esta función funciona sólo para una única curva.
+%%Esta funciÃ³n funciona sÃ³lo para una Ãºnica curva.
 
 for i=1:length(IVmeasure)
     if isfield(IVmeasure(i),'ttes')
@@ -9,7 +9,8 @@ for i=1:length(IVmeasure)
         IVstruct=GetIVTES(Circuit,IVmeasure(i),TES);
     end
 
-    plot(IVstruct.ttes,IVstruct.Rtes,'.-','DisplayName',strcat(num2str(1000*IVstruct.Tbath),'mK'));
-    xlabel('T_{tes}(K)');ylabel('R_{tes}(\Omega)');grid on,hold on
+    plot(IVstruct.ttes,IVstruct.Rtes*1e3,'.-','DisplayName',strcat(num2str(1000*IVstruct.Tbath),'mK'));
+    xlabel('T_{tes}(K)','fontsize',12);ylabel('R_{tes}(m\Omega)','fontsize',12);grid on,hold on
     legend('-DynamicLegend')
+    set(gca,'fontsize',12);
 end
