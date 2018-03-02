c75c884550693e8e9ed321ab64a5a4ee0580b0f7
carlos <carlos.pobes@gmail.com>
2018-03-02 10:27:24 +0100

    modificacion en ListInBiasOrder
    -ligero cambio en funcion para ordenar otros tipos de ficheros

--- ListInBiasOrder.m
+++ ListInBiasOrder.m
@@ -9,10 +9,12 @@ else
 end
 
 f=dir(D);
+f=f(~[f.isdir]);
 %length(f)
+f.name
 for i=1:length(f)
-    str=regexp(f(i).name,'-?\d+.?\d+uA','match');
-    Ibs(i)=sscanf(char(str),'%fuA');
+    str=regexp(f(i).name,'-?\d+.?\d*uA','match')
+    Ibs(i)=sscanf(char(str),'%fuA')
 end
 
 [ii,jj]=sort(abs(Ibs),order);
