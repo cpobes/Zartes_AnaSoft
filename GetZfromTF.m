0e04af8b0aafc0f4b233d89a3c54df1a6bc4d882
carlos <carlos.pobes@gmail.com>
2019-06-05 18:49:29 +0200

    añago funcion
    -añado GetZfromTF para poder sacar y pintr las ztes a partir de las TF.

---
+++ GetZfromTF.m
@@ -0,0 +1,24 @@
+function ztes= GetZfromTF( tf, TFS, circuit )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+if isstruct(tf)
+    tfaux=tf.tf;
+    f=tf.f
+else
+    tfaux=tf(:,2)+1i*tf(:,3);
+    f=tf(:,1);
+end
+
+if isstruct(TFS)
+    tfsaux=TFS.tf;
+else
+    tfsaux=TFS(:,2)+1i*TFS(:,3);
+end
+
+Rth=circuit.Rsh+circuit.Rpar+2*pi*circuit.L*f*1i;
+ztes.tf=(tfsaux./tfaux-1).*Rth;
+ztes.f=f;
+
+end
+
