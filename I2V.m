5fdb743dfec55f971e9ce07f62c3dc80303c6aea
carlos <carlos.pobes@gmail.com>
2019-11-12 09:50:57 +0100

    update I2V
    -cambio I2V para pasar circuit

--- I2V.m
+++ I2V.m
@@ -1,5 +1,8 @@
-function Vout=I2V(ites,Rf)
+function Vout=I2V(ites,circuit)
 %convert ites to Vout 
-invMf=66;
-invMs=24.1;
-Vout=ites.*invMf*Rf/invMs;
+%invMf=66;
+%invMs=24.1;
+Rf=circuit.Rf;
+invMf=circuit.invMf;
+invMin=circuit.invMin;
+Vout=ites.*invMf*Rf/invMin;
