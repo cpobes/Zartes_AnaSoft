1f16b3c65414c5a65d63d101f2591f9c41284a70
carlos <carlos.pobes@gmail.com>
2020-03-04 16:43:14 +0100

    updated AnalizaPulseDir

--- AnalizaPulseDir.m
+++ AnalizaPulseDir.m
@@ -28,7 +28,7 @@ opt.RL=2000;
 opt.SR=100000;
 i=1;
 cd(dir)
-for iii=ind(1000:1100);%1:N
+for iii=ind;%1:N
 try
     %file=strcat(dir,'pulso_',num2str(i));
     %pulso=importdata(strcat(dir,'\',files(i,:)));
@@ -42,18 +42,18 @@ try
     amp(i)=max(medfilt1(pulso(:,2),10))-dc(i);
     %ind=find(pulso(:,2)-dc(i)<AMPthr);%%%seleccionamos un rango que no esté saturado para hacer el ajuste.
     %ind=find(pulso(:,1)<0.12 & pulso(:,1)>0.1);
-    %[v,t]=findpeaks(pulso(:,2),pulso(:,1),'minpeakprominence',0.05)
-    v=0;t=0;
+    [v,t]=findpeaks(pulso(:,2),pulso(:,1),'minpeakprominence',0.05);
+    %v=0;t=0;
     npeaks(i)=numel(v);
     ntimes(i).times=t;
-    if(0)
-        ind=1:L;
+    if(1)
+        ind_fit=1:L;
         %p0=[0.01 0.001 1e-5];
         %fhandle=@(p,x)p(1)*(exp(-(x-t0)/p(2))-exp(-(x-t0)/p(3))).*heaviside(x-t0);%%%ojo a t0.
         p0=[0.1 0.001 1e-5 0.002];
         fhandle=@(p,x)p(1)*(exp(-(x-p(4))/p(2))-exp(-(x-p(4))/p(3))).*heaviside(x-p(4));
-        fit_pulso=lsqcurvefit(fhandle,p0,pulso(ind,1),pulso(ind,2)-dc(i));
-        area_corrected(i)=sum(fhandle(fit_pulso,pulso(ind,1)));
+        fit_pulso=lsqcurvefit(fhandle,p0,pulso(ind_fit,1),pulso(ind_fit,2)-dc(i));
+        area_corrected(i)=sum(fhandle(fit_pulso,pulso(ind_fit,1)));
         tau_rise(i)=fit_pulso(3);
         tau_fall(i)=fit_pulso(2);
         A(i)=fit_pulso(1);
@@ -69,13 +69,13 @@ catch
         amp(i)=0;
         npeaks(i)=0;
         ntimes(i).times=[];
-%         area_corrected(i)=0;
-%         tau_rise(i)=0;
-%         tau_fall(i)=0;
-%         A(i)=0;
-%         t0(i)=0;
+        area_corrected(i)=0;
+        tau_rise(i)=0;
+        tau_fall(i)=0;
+        A(i)=0;
+        t0(i)=0;
         names{i}={};
-        index(i)=[];
+        index(i)=0;
         'di error'
 end
 i=i+1;
@@ -89,10 +89,10 @@ cd ..
     PulseParameters.ntimes=ntimes;
     PulseParameters.names=names;
     PulseParameters.index=index;
-    %PulseParameters.fit_pulso=fit_pulso;
-%     PulseParameters.area_corrected=area_corrected;
-%     PulseParameters.A=A;
-%     PulseParameters.tau_rise=tau_rise;
-%     PulseParameters.tau_fall=tau_fall;
-%     PulseParameters.t0=t0;
+    PulseParameters.fit_pulso=fit_pulso;
+    PulseParameters.area_corrected=area_corrected;
+    PulseParameters.A=A;
+    PulseParameters.tau_rise=tau_rise;
+    PulseParameters.tau_fall=tau_fall;
+    PulseParameters.t0=t0;
     
