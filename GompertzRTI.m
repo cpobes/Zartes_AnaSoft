6d0e99177c5563d4bc4fd1b9e7b3e5cdee000766
cpobes <carlos.pobes@gmail.com>
2020-10-20 14:02:18 +0200

    Gompertz tests
    - Creo una funcion para representar la RTI como una Gompertz
    - Creo un script para simular las IVs con un nuevo metodo que va muy bien.

--- GompertzRTI.m
+++ GompertzRTI.m
@@ -1,16 +1,19 @@
-function Rtes=GompertzRTI(Ttes,Ites)
+function Rtes=GompertzRTI(Ttes,Ites,varargin)
 %%%Intento de usar gompertz para representar la R(T,I)
 
 if nargin==2
-    Tc0=0.1;
-    Ic0=1e-3;
+    Tc0=0.0927;
+    Ic0=1e-1;
 else
     TES=varargin{1};
     Tc0=TES.Tc0;
     Ic0=TES.Ic0;
 end
 
-a=0.04;b=0.01;c=0.01;d=1.5e-3;e=1e-4;
+%a=0.04;b=0.01;c=0.01;d=1.5e-3;e=1e-4;
+a=87.2e-3; b=1e-2;c=0;
+d=4e-3;e=1e-3;
+
 Tc=@(I)Tc0*(1-(I/Ic0).^(2/3));
 plane=@(T,I)(a+b*T+c*I);
 den=@(I)(d+e*I);
---
+++ tmp/SteadyStateScript.m
@@ -0,0 +1,31 @@
+%%%Solver script
+TtesArray=0.12:-1e-4:0.06;
+TbathArray=0.08:0.002:0.1;
+I=0:1e-8:50e-6;
+    Rsh=2e-3; RL=2.182e-3;
+    n=3.067;K=1.769e-9;
+P=@(x,y) K*(x.^n-y.^n);
+for k=1:length(TbathArray)
+    Tbath=TbathArray(k);
+    Ites=[];Rtes=[];Ibias=[];
+for i=1:length(TtesArray)
+    Ttes=TtesArray(i); 
+    if Ttes<=TbathArray(k) break;end
+    %P=K*(Ttes.^n-Tbath.^n);
+    pw=P(Ttes,Tbath);
+    F=@(I) I.^2.*GompertzRTI(Ttes,I)-pw;
+    %Ites(i)=fsolve(F,5e-6);
+    m=min(abs(F(I)));
+    j=find(abs(F(I))==m);
+    Ites(i)=I(j(1));
+    Rtes(i)=GompertzRTI(Ttes,Ites(i));
+    G=@(I) I.*(RL+GompertzRTI(Ttes,I));
+    Ibias(i)=G(Ites(i))/Rsh;
+end
+IV(k).Ttes=TtesArray(1:length(Rtes));
+IV(k).Ites=Ites;
+IV(k).Rtes=Rtes;IV(k).Rtes(Ites==0)=0;%%%Cuando Ttes<Tb ->Ites=0
+IV(k).Vtes=Rtes.*Ites;
+IV(k).Ptes=Rtes.*Ites.^2;
+IV(k).ibias=Ibias;
+end
