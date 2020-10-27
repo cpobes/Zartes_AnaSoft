5f8f542284c45064b9a6bd354954f91a12420f41
cpobes <carlos.pobes@gmail.com>
2020-10-27 13:46:25 +0100

    refix bug
    - el centrado IVs tenia un bug dificil de detectar. Había cambiado spline por fit, pero tenía seleccionado el rango de abs(Ib)<10uA y las curvas mas altas no son totalmente rectas ahi y el fit falla.

--- CentrarIVs.m
+++ CentrarIVs.m
@@ -11,8 +11,9 @@ for i=1:length(RawIVset)
     ind=find(sign(RawIVset(i).ibias(1))*(RawIVset(i).ibias)<0);%%%seleccionamos los puntos que pasan al otro lado.
     
     if isempty(ind)
-        ind=find(abs(RawIVset(i).ibias*1e6)<20);%%%seleccionamos el rango de corrientes cercano a 0.
-        %%%Ojo pq según que ibias se use, puede estar en A o uA. 
+        ind=find(abs(RawIVset(i).ibias)<10e-6);%%%seleccionamos el rango de corrientes cercano a 0.
+        %%%Ojo pq según que ibias se use, puede estar en A o uA. El
+        %%%importRawIVs convierte los datos (uA) a A. 
     end
     %ind
     
@@ -25,9 +26,10 @@ for i=1:length(RawIVset)
     vaux=RawIVset(i).vout(ind);
     vaux=vaux(iii);
         if isnan(voff)
-        %voff=spline(iaux,vaux,ioff);
-        ff=fit(iaux,vaux,fittype('poly1')); %%%El spline vale si el punto está dentro del rango de ajuste.
-        voff=ff(ioff);
+        voff=spline(iaux,vaux,ioff);
+        %ff=fit(iaux,vaux,fittype('poly1')); %%%El spline vale si el punto está dentro del rango de ajuste.
+        %voff=ff(ioff);%%%Pero el fit falla en las IVs más altas pq por
+        %debajo de 10uA la IV no es recta!
         end
     IVset(i).ibias=RawIVset(i).ibias-ioff;
     IVset(i).vout=RawIVset(i).vout-voff;
