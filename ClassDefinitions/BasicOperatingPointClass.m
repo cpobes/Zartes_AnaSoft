05582ca8ac8066436ba7ec19d768348305880f43
cpobes <carlos.pobes@gmail.com>
2021-04-08 14:37:05 +0200

    v0 de basic operating point

---
+++ ClassDefinitions/BasicOperatingPointClass.m
@@ -0,0 +1,17 @@
+classdef BasicOperatingPointClass < handle
+    
+    properties
+        %%%Direct Experimental
+        fTbath=0.1;
+        fIbias=100e-6;
+        %%%Deduced
+        fR0=0;
+        fI0=0;
+        fV0=0;
+        fT0=0;
+        fP0=0;
+    end
+    
+    methods
+    end
+end
