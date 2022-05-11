63436ec5bf521a40e6f560ef8f27dedff43232ae
cpobes <carlos.pobes@gmail.com>
2022-05-11 17:26:35 +0200

    Update TF_HP.m
    cambio formato de la funcion para calcular la TF de estado N y S.

--- TF_HP.m
+++ TF_HP.m
@@ -1,20 +1,28 @@
-function [TF,f]=TF_HP(Rtes,Rf,L)
+function TF=TF_HP(Rtes,circuit)
 %expected TF from the HP.TF=Vout/Vin
 %En primera aproximación Zbox=Rk, pero si se tiene en cuenta la Lbox cambia
 %un poco la TF esperada. También se puede añadir la RC en paralelo y se
 %nota también un poco, pero sólo a alta frecuencia. 
 %sólo funciona para estado normal y superconductor del TES.
 
-invMs=24.1;
-invMf=66;
-Rsh=2e-3;
-Rpar=0.11e-3;
+f=logspace(1,5,1e4);
+
+invMs=circuit.invMin;
+invMf=circuit.invMf;
+Rsh=circuit.Rsh;
+Rpar=circuit.Rpar;
 Rth=Rsh+Rpar;
+Rf=circuit.Rf;
+L=circuit.L;
+
 Rk=1e4;
 Lbox=2e-3;
-f=logspace(1,6,100);
 %Zc de la ibox.
 Rc=200;Cc=100e-12;
 Zc=Rc+1./(2*pi*1i*f*Cc);
 Zbox=Rk*Zc./(Rk+Zc)+2*pi*1i*f*Lbox;
-TF=Rsh*Rf*(invMf/invMs)./Zbox./(Rth+Rtes+1i*2*pi*f*L)./(1+Rk./Zc);
+
+TF.tf=Rsh*Rf*(invMf/invMs)./Zbox./(Rth+Rtes+1i*2*pi*f*L)./(1+Rk./Zc);
+TF.f=f;
+TF.re=real(TF.tf);
+TF.im=imag(TF.tf);
