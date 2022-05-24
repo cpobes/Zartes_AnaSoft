6a69b0b35b734289cc54893aa091de19ff488e7e
cpobes <carlos.pobes@gmail.com>
2022-05-24 14:11:25 +0200

    Update CheckTF.m
    añado codigo color a la frecuencia (log) para ver en que sentido se recorre la curva. uso patch. ojo pq no parece que se borre con hold off.

--- CheckTF.m
+++ CheckTF.m
@@ -1,5 +1,13 @@
 function CheckTF(circuit)
 tfn=TF_HP(circuit.Rn,circuit);
 tfs=TF_HP(0,circuit);
-plot(tfn.tf,'k','linewidth',2)
-plot(tfs.tf,'m','linewidth',2)
+
+xN=tfn.re;yN=tfn.im;
+xS=tfs.re;yS=tfs.im;
+f=tfn.f;
+
+%plot(tfn.tf,'k','linewidth',2)
+%plot(tfs.tf,'m','linewidth',2)
+patch([xN nan],[yN nan],[log10(f) nan],[log10(f) nan],'edgecolor','interp','linewidth',2);
+patch([xS nan],[yS nan],[log10(f) nan],[log10(f) nan],'edgecolor','interp','linewidth',2);
+colorbar;grid on
